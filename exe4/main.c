87457f6835923f6232ac0ead9eecb82d83d27abb
joaovabbr <joaovab@al.insper.edu.br>
2025-02-20 11:12:21 -0300

    .

--- exe4/main.c
+++ exe4/main.c
@@ -19,7 +19,26 @@ void btn_callback(uint gpio, uint32_t events) {
 
 int main() {
   stdio_init_all();
+  gpio_init(BTN_PIN_R);
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(LED_PIN_G);
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
   while (true) {
+    if (BTN_FLAG_R){
+      gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+      BTN_FLAG_R = 0;
+    }
+    if (BTN_FLAG_G){
+      gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
+      BTN_FLAG_G = 0;
+    }
   }
 }
