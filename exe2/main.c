3ffeb4cb4cb96665ad4aa1dc0279ab7e387c770d
joaovabbr <joaovab@al.insper.edu.br>
2025-02-20 11:27:39 -0300

    .

--- exe2/main.c
+++ exe2/main.c
@@ -1,8 +1,8 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
-const int BTN_PIN;
-const int LED_PIN;
+const int BTN_PIN = 28;
+const int LED_PIN = 4;
 volatile int BTN_FLAG;
 
 void btn_callback(uint gpio, uint32_t events) {
@@ -22,8 +22,11 @@ int main() {
 
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_set_irq_enabled_with_callback(BTN_PIN,GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE,true, &btn_callback);
   while (true) {
     if (BTN_FLAG){
+      gpio_put(LED_PIN, 1);
+    }else{
       gpio_put(LED_PIN, 0);
     }
   }
